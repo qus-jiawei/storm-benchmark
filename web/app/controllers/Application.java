835386fcc9d4f9eed9c2f0d71aba5d7555da729f
qus-jiawei <qus.jiawei@gmail.com>
2013-09-04 14:20:49 +0800

    fix change file name bug

--- web/app/controllers/Application.java
+++ web/app/controllers/Application.java
@@ -14,33 +14,42 @@ import views.html.playhelp;
 import com.google.common.collect.Lists;
 
 public class Application extends Controller {
-  
-    public static Result index() {
-    	String[] chosenFileList = request().queryString().get("csv");
-    	
-//    	ChartData
-    	ChartDataBuilder builder = new ChartDataBuilder(){
+
+	public static Result index() {
+		String[] chosenFileList = request().queryString().get("csv");
+		if (chosenFileList != null) {
+			for (int i = 0; i < chosenFileList.length; i++) {
+				chosenFileList[i] = Helper.displayToFile(chosenFileList[i]);
+			}
+		}
+		// ChartData
+		ChartDataBuilder builder = new ChartDataBuilder() {
 			@Override
 			public Integer valueOf(String temp) {
 				return Integer.valueOf(temp);
 			}
-    	};
-    	List<String> xAxis = Lists.newArrayList();
-    	for(int i =0;i<200;i++){
-    		xAxis.add( (i*100)+"ms");
-    	}
-    	ChartData tupleChartData = builder.getChartData("tuple处理量","条",chosenFileList,1,xAxis);
-    	ChartData sizeChartData = builder.getChartData("数据处理量","K",chosenFileList,2,xAxis);
-    	ChartData avgChartData = builder.getChartData("平均处理延迟","ms",chosenFileList,3,xAxis);
-    	ChartData maxChartData = builder.getChartData("最大处理延迟","ms",chosenFileList,4,xAxis);
-    	ChartData minChartData = builder.getChartData("最小处理延迟","ms",chosenFileList,5,xAxis);
-    	return ok(index.render(Helper.getDataFileList(),Arrays.toString(chosenFileList),
-    			tupleChartData,sizeChartData,avgChartData,maxChartData,minChartData));
-    }
-    
-    public static Result playhelp() {
-    	return ok(playhelp.render("Your new application is ready."));
-    }
-    
-    
+		};
+		List<String> xAxis = Lists.newArrayList();
+		for (int i = 0; i < 200; i++) {
+			xAxis.add((i * 100) + "ms");
+		}
+		ChartData tupleChartData = builder.getChartData("tuple处理量", "条",
+				chosenFileList, 1, xAxis);
+		ChartData sizeChartData = builder.getChartData("数据处理量", "K",
+				chosenFileList, 2, xAxis);
+		ChartData avgChartData = builder.getChartData("平均处理延迟", "ms",
+				chosenFileList, 3, xAxis);
+		ChartData maxChartData = builder.getChartData("最大处理延迟", "ms",
+				chosenFileList, 4, xAxis);
+		ChartData minChartData = builder.getChartData("最小处理延迟", "ms",
+				chosenFileList, 5, xAxis);
+		return ok(index.render(Helper.getDataFileList(),
+				Arrays.toString(chosenFileList), tupleChartData, sizeChartData,
+				avgChartData, maxChartData, minChartData));
+	}
+
+	public static Result playhelp() {
+		return ok(playhelp.render("Your new application is ready."));
+	}
+
 }
--- web/app/lib/Helper.java
+++ web/app/lib/Helper.java
@@ -4,22 +4,42 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 public class Helper {
-	
-	/** define config here
+
+	/**
+	 * define config here
 	 * 
 	 */
 	static private final String dataDir = "/home/qiujw/storm_test/collect_data";
-//	static private final String dataDir = "C:/Users/Administrator/Desktop/storm/测试/测试结果";
-	
+
+	// static private final String dataDir =
+	// "C:/Users/Administrator/Desktop/storm/测试/测试结果";
+
 	static public String getDataDir() {
 		return dataDir;
 	}
-	static public List<String> getDataFileList(){
+
+	static public List<String> getDataFileList() {
 		File dir = new File(getDataDir());
-		if(dir.exists()){
-			return Arrays.asList(dir.list());
+		if (dir.exists()) {
+			List<String> re = Lists.newArrayList();
+			String[] temp = dir.list();
+			if (temp != null) {
+				for (String t : temp) {
+					re.add(fileToDisplay(t));
+				}
+			}
+			return re;
 		}
 		return null;
+	}
+	
+	static public String fileToDisplay(String file){
+		return file.replaceAll("_", " ").substring(0,file.length()-4);
+	}
+	static public String displayToFile(String display){
+		return display.replaceAll(" ", "_")+".csv";
 	}
 }
